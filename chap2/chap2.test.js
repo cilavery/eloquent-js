cd0b82056ff8f44b2ea147dae6185093c20070ff
Cecilia Avery <cilsong@gmail.com>
2018-10-17 16:39:04 -0400

    chap2-fizzBuzz

--- chap2/chap2.test.js
+++ chap2/chap2.test.js
@@ -1,4 +1,7 @@
 const loopingTriangle = require('./loopingTriangle.js');
+const fizzBuzz = require('./fizzBuzz.js');
+
+/* How to test console.logs? */
 
 //test's test
 function sum (a,b) {
@@ -13,4 +16,10 @@ test ('adds 1 + 2 to equal 3', () => {
 //looping triangle
 test('prints a triangle of hashes', () => {
   expect(loopingTriangle(7)).toHaveLength(7);
+});
+
+//fizzBuzz
+
+test('prints "fizz" with 3', () => {
+  expect(fizzBuzz.fizzBuzz(3)).toMatch('Fizz');
 });
---
+++ chap2/fizzBuzz.js
@@ -0,0 +1,16 @@
+module.exports.fizzBuzz = function (num) {
+  let fizz = 'Fizz';
+  let buzz = 'Buzz';
+  for (let i = 1; i <= num; i++) {
+    if (i % 15 === 0) {
+      console.log(fizz+buzz);
+    } else if (i % 3 === 0) {
+      console.log(fizz);
+    } else if (i % 5 === 0) {
+      console.log(buzz);
+
+    }
+  }
+}
+
+
